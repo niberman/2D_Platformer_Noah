1178e00e758a24c837dc41c2f6d1b7cae3a573ac
Noah Berman <noahberman14@gmail.com>
2021-02-17 16:51:32 -0700

    flip

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,10 +10,13 @@ public class Player : MonoBehaviour
 
     private float movementSpeed;
 
+    private bool facingRight;
+
     // Start is called before the first frame update
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
+        facingRight = true;
     }
 
     // Update is called once per frame
@@ -28,8 +31,22 @@ public class Player : MonoBehaviour
         HandleMovement(horizontal);
     }
 
-    private void HandleMovement(float horizontal)
+    public void HandleMovement(float horizontal)
     {
         myRigidbody.velocity = new Vector2(horizontal * movementSpeed, myRigidbody.velocity.y);
     }
+
+    private void Flip(float horizontal)
+    {
+        if(facingRight == true && horizontal < 0)
+        {
+            facingRight = !facingRight;
+            return;
+        }
+    }
+    /*Update runs once per frame, fixed update runs as many times per frame as you want
+     * fixed update works with the physics engine, so while using a rigidbody, you should use fixed
+     */
+
+
 }
