7c8289f50ae784f3d4c2d7c3a2bfd2e44394192d
Noah Berman <noahberman14@gmail.com>
2021-02-19 14:21:20 -0700

    camera work

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float xMin;
+    public float xMax;
+    public float yMin;
+    public float yMax;
+
+    private GameObject cameraTarget;
+    private Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        cameraTarget = GameObject.Find("Player");
+        target = cameraTarget.transform;
+        transform.position = new Vector3(Mathf.Clamp(target.position.x, xMin, xMax),
+            Mathf.Clamp(target.position.y, yMin, yMax), transform.position.z);
+    }
+}
