19b880d08ffa71331cd1139cd5ac4318e461855c
Noah Berman <noahberman14@gmail.com>
2021-03-11 13:47:04 -0700

    not working

---
+++ Assets/Scripts/FollowPath.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPath : MonoBehaviour
+{
+    public enum FollowType
+    {
+        MoveTowards,
+        Lerp,
+        Falling
+    }
+
+    public FollowType Type = FollowType.MoveTowards;
+    public PathDefinition Path;
+    public float Speed = 1;
+    public float MaxDistanceToGoal = .1f;
+
+    private IEnumerator<Transform> _currentPoint;
+
+    public void Start()
+    {
+        
+    }
+
+    public void Update()
+    {
+        
+    }
+
+}
---
+++ Assets/Scripts/PathDefinition.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathDefinition : MonoBehaviour
+{
+    public Transform[] Points;
+
+    public IEnumerator<Transform> GetPathsEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnDrawGizmos()
+    {
+        if (Points = null || Points < 1)
+        {
+            return;
+        }
+        foreach(var i in Points; i = 1; i++)
+        {
+            Gizmos.DrawLine(Points[i - 1].position, Points[i].position);
+        }
+
+    }
+}
